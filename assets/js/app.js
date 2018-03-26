985dbdd982c7739dd5657c874b510abc5e65e237
bimiriya <poyarzunaspee@gmail.com>
2018-03-26 14:46:48 -0300

    projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,8 @@ $(document).ready(() => {
   showItemTitle();
   getSkills();
   init();
-  $('#navwide a').on('click', smoothScroll);
+  $('nav a[href*="#"]').click(smoothScroll);
+  $('#navwide a[href*="#"]').click(smoothScroll);
 });
 
 function fixNavHeight() {
@@ -47,28 +48,36 @@ function getSkills() {
 }
 
 function smoothScroll(event) {
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== '') {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, #contents').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800,
-      function() {
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      }
-    );
-  } // End if
-}
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname === this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 0, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(':focus')) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+};
 
 function init() {
   var speed = 330,
