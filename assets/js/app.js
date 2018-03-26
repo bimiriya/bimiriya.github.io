29fb74106d2ef0caa2abeb12bf278df515c834e7
bimiriya <poyarzunaspee@gmail.com>
2018-03-26 16:53:54 -0300

    projects modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,8 @@
 $(document).ready(() => {
   fixNavHeight();
-  fixProfileHeight();
   showItemTitle();
-  getSkills();
+  // getSkills();
   init();
-  $('nav a[href*="#"]').click(smoothScroll);
-  $('#navwide a[href*="#"]').click(smoothScroll);
 });
 
 function fixNavHeight() {
@@ -14,15 +11,6 @@ function fixNavHeight() {
 
   $(window).resize(function() {
     $('.navitem').height($(document).height() / 5.1 + 'px');
-  });
-}
-
-function fixProfileHeight() {
-  let widthtLal = $('.lala').width();
-  $('.lala').height(widthtLal + 'px');
-
-  $('.lala').resize(function() {
-    $('.lala').height($('.lala').width());
   });
 }
 
@@ -47,38 +35,6 @@ function getSkills() {
   $('#skill-set i').css('font-size', skillWidth + 'px');
 }
 
-function smoothScroll(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname === this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 0, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(':focus')) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
-    }
-  }
-};
-
 function init() {
   var speed = 330,
     easing = mina.backout;
@@ -98,4 +54,14 @@ function init() {
       path.animate({ 'path': pathConfig.from }, speed, easing);
     });
   });
+}
+
+$('#projects button').click(showProject);
+
+function showProject(event) {
+  let github = $(event.target).data('github');
+  let demo = $(event.target).data('demo');
+  $('#viewProject').modal();
+  $('#github').attr('href', github);
+  $('#demo').attr('href', demo);
 }
